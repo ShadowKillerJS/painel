82b2fe4650cece8b17d951adaa80f41a382f1238
Shadow <davidkingpanda@gmail.com>
2023-02-18 19:10:44 -0300

    novos

---
+++ src/Services/index.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: 'https://backcx.azurewebsites.net',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  }, 
+  timeout: 3000,
+})
+
+instance.interceptors.request.use(function (config) {
+    const token = localStorage.getItem('Auth');
+    config.headers.Authorization = token;
+
+    return config;
+});
+
+// export const loadToken = (token) => {
+//   try{
+//     instance.defaults.headers.common['Authorization'] = token ? 'Bearer ' + token : null
+//   }catch(err){
+//     console.log('Erro token', err)
+//   }
+// }
+
+export default instance
